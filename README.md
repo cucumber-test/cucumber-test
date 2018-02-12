2b5cb79309002887f3779bde68656519b8e98012
wharsojo <wharsojo@gmail.com>
2018-02-12 10:15:46 +0800

    add appium android testing

--- README.md
+++ README.md
@@ -27,6 +27,44 @@ Scenario: Search cucumber-test
     And I press "Enter"
     Then I expect that element "a[href='https://cucumber.io/']" becomes visible
 ```
+## Appium
+### Android Device
+please check the developer tools security options, ensure them to be checked.
+* USB debugging
+* USB debugging(Security settings)
+
+```cucumber
+@simple
+Feature: Search on Google
+    Search should be on the google website
+    and the first should be cucumber.io
+
+Scenario: Navigate to Google
+    Given I open the url "https://www.google.com"
+    Then I expect that the title is "Google"
+
+@chrome
+Scenario: Search cucumber-test
+    When I set "cucumber-test" to the inputfield "[name=q]"
+    And I expect that element "[name=q]" becomes visible
+    When I click on the button "input.lsb"
+    Then I expect that element "a[href='https://cucumber.io/']" becomes visible
+
+@iphone
+Scenario: Search cucumber-test
+    When I set "cucumber-test" to the inputfield "[name=q]"
+    And I expect that element "[name=q]" becomes visible
+    When I click on the button "[name=btnG]"
+    Then I expect that element "a[href='https://cucumber.io/']" becomes visible
+```
+```bash
+# Install appium server
+npm install -g appium appium-doctor
+# run appium & connect your android device using USB
+appium
+# check deviceName - adb devices & pass to deviceName:android version
+cct -t '@simple and not @chrome' --android f344ee26:7.0
+```
 
 ## Integration with: Sauce Labs
 
--- src/cct.js
+++ src/cct.js
@@ -1,9 +1,9 @@
 const program = require('commander');
-const { Launcher } = require('webdriverio');
+const { Launcher, remote } = require('webdriverio');
 
 console.log('Loading...');
 
-program.version('1.0.7');
+program.version('1.0.8');
 program.on('-h, --help', function() {
     console.log('  Examples:');
     console.log('');
@@ -16,6 +16,7 @@ program.option('-s, --souce', 'Run in Souce labs cloud service');
 program.option('-i, --instances [instances]', 'Max Instances');
 program.option('-b, --browser [browser]', 'Target Browser');
 program.option('--iphone', 'Chrome simulate iPhone');
+program.option('--android [android]', 'Android');
 
 program.parse(process.argv);
 
@@ -29,22 +30,46 @@ const options = {
     }
 };
 
-const instances = +(program.instances || 1);
-const browser = program.browser || 'chrome';
-console.log('Browser:', browser, program.iphone ? '--iphone' : '');
-options.maxInstances = instances;
-options.capabilities = browser.split(',').map(x => {
-    const bconfig = {
-        maxInstances: 5,
-        browserName: x
-    };
-    if (x==='chrome' && program.iphone) {
-        bconfig.chromeOptions = {
-            args: ['use-mobile-user-agent', 'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3']
+let browser = 'chrome';
+options.maxInstances = +(program.instances || 1);
+if (program.android) {
+    const android = program.android.split(':');
+    options.port = '4723';
+    options.services = ['appium'];
+    options.capabilities = [{
+        appiumVersion: '1.7.2',                    // Appium module version
+        browserName: browser,                      // browser name is empty for native apps
+        platformName: 'Android',
+        platformVersion: android[1] || '7.0',      // Android platform version of the device
+        deviceName: android[0],                    // device name of the mobile device
+        newCommandTimeout: 30 * 60000,
+        waitforTimeout: 10000,
+        commandTimeout: 7200,
+        // app: './app/LGCalculator.apk',          // Path to the native app
+        // appPackage: 'com.android.calculator2',  // Package name of the app
+        // appActivity: 'com.android.calculator2.Calculator', // App activity of the app
+    }];
+} else {
+    browser = program.browser || 'chrome';
+    options.services = ['selenium-standalone', 'sauce'];  // 'firefox-profile'
+    options.capabilities = browser.split(',').map(x => {
+        const bconfig = {
+            maxInstances: 5,
+            browserName: x
+        };
+        if (x==='chrome' && program.iphone) {
+            bconfig.chromeOptions = {
+                args: ['use-mobile-user-agent', 'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3']
+            }
         }
-    }
-    return bconfig;
-})
+        return bconfig;
+    });
+}
+
+console.log('Browser:', browser,
+program.iphone ? '--iphone' : '',
+program.android ? '--android' : '',
+);
 
 if (program.souce) {
     options.sauceConnect = true;
