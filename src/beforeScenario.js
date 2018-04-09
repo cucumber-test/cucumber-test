69f9f9191d18289fc1dc5e53c4d779eedbe2151d
wharsojo <wharsojo@gmail.com>
2018-04-09 15:18:23 +0800

    fix custom tags

--- src/beforeScenario.js
+++ src/beforeScenario.js
@@ -3,28 +3,29 @@ module.exports = event => {
     const {tags, vars} = global;
     let isRemove = false;
 
-    event.tags.forEach(tag => {
+    for (let tag of event.tags) {
         const arr = tag.name.split(':');
         const fn = tags[arr[0]];
 
-        if (!isRemove && arr[1]) {
+        if (arr[1]) {
             if (url.match(arr[1])) {
                 isRemove = (arr[0]==='@__non_url');
             } else {
                 isRemove = (arr[0]==='@__url');
             }
         }
+        if (!isRemove && fn) {
+            console.log('>>>>>',arr[0]);
+            isRemove = !fn(browser, global.vars, arr[1]);
+        }
         if (isRemove) {
             event.tags = [];
             event.name = '';
             event.steps = [];
-        } else {
-            if (fn) {
-                console.log('>>>>>',arr[0]);
-                isRemove = !fn(browser, global.vars, arr[1]);
-            }
+            break;
         }
-    })
+    }
+
     // variable parser
     event.steps.forEach(step => {
         const varNames = step.name.match(/\${([A-z0-9_.]+)}/g);
