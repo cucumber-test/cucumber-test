0271f5c6afa6d5a063c5cbbbf431f98c42321a67
wharsojo <wharsojo@gmail.com>
2018-04-23 06:38:31 +0800

    update setInputFiield

--- flib/share.feature
+++ flib/share.feature
@@ -7,21 +7,21 @@ Scenario: Navigate to Google
 
 @__non_mobile @__chrome @__non_url:localhost
 Scenario: Search cucumber-test
-    When I type "${g.search}" to the inputfield "${g.q}"
+    When I set "${g.search}" to the inputfield "${g.q}"
     And I expect that element "${g.btnG}" becomes visible
     And I click on the button "${g.btnG}"
     Then I expect that element "a[href='https://cucumber.io/']" becomes visible
 
 @__non_mobile @__non_chrome
 Scenario: Search cucumber-test
-    When I type "${g.search} *" to the inputfield "${g.q}"
+    When I set "${g.search} *" to the inputfield "${g.q}"
     And I expect that element "${g.btnG}" becomes visible
     And I click on the button "${g.btnG}"
     Then I expect that element "a[href='https://en.wikipedia.org/wiki/Cucumber_(software)']" becomes visible
 
 @__mobile
 Scenario: Search cucumber-test
-    When I type "cucumber-test mobile" to the inputfield "${g.q}"
+    When I set "cucumber-test mobile" to the inputfield "${g.q}"
     And I expect that element "${g.btnM}" becomes visible
     And I click on the button "${g.btnM}"
     Then I expect that element "a[href='https://cucumber.io/']" becomes visible
--- src/support/action/setInputField.js
+++ src/support/action/setInputField.js
@@ -1,4 +1,4 @@
-import checkIfElementExists from '../lib/checkIfElementExists';
+// import checkIfElementExists from '../lib/checkIfElementExists';
 
 /**
  * Set the value of the given input field to a new value or add a value to the
@@ -12,25 +12,10 @@ module.exports = (method, value, element) => {
      * The command to perform on the browser object (addValue or setValue)
      * @type {String}
      */
-
-    const {browserName} = browser.desiredCapabilities;
-
-    let checkValue = value;
-    if (!value) {
-        checkValue = '';
-    }
-
+    let checkValue = value || '';
     const { base } = global.browser.options;
+    const command = (method === 'add') ? 'addValue' : 'setValue';
     browser.pause(base.inputPause || 500, '*internal*');
-    // const windowHandles = browser.windowHandles();
-    // if (browserName==='safari' && windowHandles.value.length>1) {
-    //     // http://www.paraesthesia.com/archive/2010/01/29/the-difference-between-a-workaround-and-a-fix.aspx/
-    //     browser.execute(function(element, checkValue) {
-    //          document.querySelector(element).value = checkValue;
-    //     }, element, checkValue)
-    // } else {
-        const command = (method === 'add') ? 'addValue' : 'setValue';
-        checkIfElementExists(element, false, 1);
-        browser[command](element, checkValue);
-    // }
+    // checkIfElementExists(element, false, 1);
+    browser[command](element, checkValue);
 };
